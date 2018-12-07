fc46a8172813264b4a177f2cd1030150ea23babf
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:06:15 +0600

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,16 @@
+﻿CREATE procedure [dbo].[AddCentreAminRegP]
+(
+	@Name nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Mobile nvarchar(50),
+	@Address nvarchar(100),
+	@Organization nvarchar(50),
+	@EmisCode nvarchar(50),
+	@ExamCategory nvarchar(50),
+	@SuperAdminCode nvarchar(50)
+)
+as
+begin
+	Insert into CenATReg values (@Name,@Email,@Password,@Mobile,@Address,@Organization,@EmisCode,@ExamCategory,@SuperAdminCode)
+End
