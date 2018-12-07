3a1f057f6dcf96ba4878231032d3dacc57e9eab4
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:05:52 +0600

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,16 @@
+﻿CREATE procedure [dbo].[AddSuperAminRegP]
+(
+	@Name nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Mobile nvarchar(50),
+	@Address nvarchar(100),
+	@GovtVerifiedCode nvarchar(50),
+	@AdminCode nvarchar(50),
+	@CentreCode nvarchar(50),
+	@ExamCategory nvarchar(50)
+)
+as
+begin
+	Insert into SupATReg values (@Name,@Email,@Password,@Mobile,@Address,@GovtVerifiedCode,@AdminCode,@CentreCode,@ExamCategory)
+End
