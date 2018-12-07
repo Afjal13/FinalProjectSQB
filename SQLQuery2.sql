0a679c4d48889fd681baecf75d3de6b555bc41a2
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:05:08 +0600

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,16 @@
+﻿create procedure [dbo].[AddAminRegP]
+(
+	@Name nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Mobile nvarchar(50),
+	@Address nvarchar(50),
+	@Organization nvarchar(50),
+	@Admincode nvarchar(50),
+	@Personalcode nvarchar(50),
+	@Examcategory nvarchar(50)
+)
+as
+begin
+	Insert into SubATReg(Name,Email,Password,Mobile,Address,Organization,Admincode,Personalcode,Examcategory) values (@Name,@Email,@Password,@Mobile,@Address,@Organization,@Admincode,@Personalcode,@Examcategory)
+End
