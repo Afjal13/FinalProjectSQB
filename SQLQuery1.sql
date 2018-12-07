d2fc5585f45b2da6cd42a01180f1df142ae86bb3
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:04:41 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿Create  procedure [dbo].[SubAdminReg]
+(
+	@Name nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Mobile nvarchar(50),
+	@Address nvarchar(100),
+	@Organization nvarchar(50),
+	@Admincode nvarchar(50),
+	@Personalcode nvarchar(50),
+	@Examcategory nvarchar(50)
+)
+as
+begin
+	Insert into SuATReg(Name,Email,Password,Mobile,Address,Organization,Admincode,Personalcode,Examcategory) values(@Name,@Email,@Password,@Mobile,@Address,@Organization,@Admincode,@Personalcode,@Examcategory)
+End
