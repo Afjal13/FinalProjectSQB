d4172f99b3dbd7b3a8f1c08e09c44c66b22ae8f0
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:05:30 +0600

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,16 @@
+﻿CREATE procedure [dbo].[AddAdRegP]
+(
+	@Name nvarchar(50),
+	@Email nvarchar(50),
+	@Password nvarchar(50),
+	@Mobile nvarchar(50),
+	@Address nvarchar(100),
+	@SuperAdminCode nvarchar(50),
+	@PersonalCode nvarchar(50),
+	@Organization nvarchar(50),
+	@ExamCategory nvarchar(50)
+)
+as
+begin
+	Insert into AdTReg values (@Name,@Email,@Password,@Mobile,@Address,@SuperAdminCode,@PersonalCode,@Organization,@ExamCategory)
+End
