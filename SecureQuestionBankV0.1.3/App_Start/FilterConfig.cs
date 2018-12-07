0b5c2892dbca059694ef2cdde0b5fb5d4258eafc
Afjal Hossain <32141447+Afjal13@users.noreply.github.com>
2018-12-07 12:46:18 +0600

    Add files via upload

---
+++ SecureQuestionBankV0.1.3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SecureQuestionBankV0._1._3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
